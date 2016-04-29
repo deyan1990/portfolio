573506ff4f2ccacfc15ae2d606e5df0b61d1939a
J0hnyBG <ivan.parvanov1@gmail.com>
2016-04-29 16:54:25 +0300

    nav is no longer img, fixes on Portfolio & Pictures of Places
    Fixed menu - no longer a list of images. .portfolio-box no longer needed

--- src/Portfolio.php
+++ src/Portfolio.php
@@ -7,9 +7,9 @@ $comments = $db->query("SELECT * FROM comments ORDER BY project_id ASC");
 while ($kea_projects = $projects->fetchObject()){
 
     ?>
-<div class = "folio"><a href="project.php?id=<?php echo $kea_projects->project_id;?>"><div class = "portfolio-box border-class"><?php echo $kea_projects->header;?><br>
+<a href="project.php?id=<?php echo $kea_projects->project_id;?>"><!--<div class="folio border-class">--><div class = "folio border-class portfolio-box"><?php echo $kea_projects->header;?><br>
     <img class="img-inside-h2" src="img/<?php  echo $kea_projects->image?>">
-    </div></div></a>
+</div><!--</div>--></a>
 <?php
 $project_id = $kea_projects->project_id;
 }
@@ -17,10 +17,10 @@ $project_id = $kea_projects->project_id;
 
 
 ?>
-  
+
 
   <!-- end .container -->
  </div>
-    
+
 </body>
 </html>
